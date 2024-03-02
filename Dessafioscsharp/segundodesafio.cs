fb1a59c5648cec3c42a126e5eb9986abb124061f
Yomisawa <130817740+Yomisawa@users.noreply.github.com>
2024-03-01 21:19:50 -0600

    Add files via upload

---
+++ Dessafioscsharp/segundodesafio.cs
@@ -0,0 +1,44 @@
+﻿﻿using System;
+
+namespace segundodesafio
+{
+    public class Calculadora
+    {
+        public int Sumar(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public int Restar(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public int Multiplicar(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public int Dividir(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+    }
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora miCalculadora = new Calculadora();
+
+            int resultadoSuma = miCalculadora.Sumar(10, 5);
+            int resultadoResta = miCalculadora.Restar(10, 5);
+            int resultadoMultiplicacion = miCalculadora.Multiplicar(10, 5);
+            int resultadoDivision = miCalculadora.Dividir(10, 5);
+
+            Console.WriteLine("Suma: " + resultadoSuma);
+            Console.WriteLine("Resta: " + resultadoResta);
+            Console.WriteLine("Multiplicación: " + resultadoMultiplicacion);
+            Console.WriteLine("División: " + resultadoDivision);
+            Console.Read();
+        }
+    }
+}
---
+++ Dessafioscsharp/tercerdesafio.cs
@@ -0,0 +1,39 @@
+﻿﻿using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            int resultado = SumarNumeros();
+            Console.WriteLine("La suma es: " + resultado);
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error inesperado: " + ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Operación completada.");
+        }
+
+        Console.ReadLine();
+    }
+    public static int SumarNumeros()
+    {
+        Console.WriteLine("Ingrese el primer número:");
+        string input1 = Console.ReadLine();
+        Console.WriteLine("Ingrese el segundo número:");
+        string input2 = Console.ReadLine();
+
+        int num1 = Convert.ToInt32(input1);
+        int num2 = Convert.ToInt32(input2);
+
+        int suma = num1 + num2;
+        return suma;
+    }
+}
